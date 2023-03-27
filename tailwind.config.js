cef7f0b59b97f5fad10cf38cb8825e52b957ee4e
Oluwatoyosi <oladipooluwatoyosi@yahoo.com>
2023-03-27 22:26:56 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
